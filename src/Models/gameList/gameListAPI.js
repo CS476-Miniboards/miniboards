0526ec762c39f564114463f5dc7406673e69a7b6
Bradley Bateman <bbateman@sgi.sk.ca>
2022-10-17 12:12:37 -0600

    Cleanup - reorg

---
+++ src/Models/gameList/gameListAPI.js
@@ -0,0 +1,12 @@
+// A mock function to mimic making an async request for data
+export function fetchCount(amount = 1) {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve({ data: amount }), 500)
+  );
+}
+
+export function fetchGameList() {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve({ data: amount }), 500)
+  );
+}
