f5f1afb25c58a4269414767c292df0d940033d3e
Bradley Bateman <bbateman@sgi.sk.ca>
2022-11-19 21:31:53 -0600

    fix warnings

--- src/Models/auth/AuthContext.js
+++ src/Models/auth/AuthContext.js
@@ -1,8 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
 import { auth } from "../../firebase";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-const newAuth = getAuth();
 
 const AuthContext = React.createContext();
 
